8035064fbf03dbcf1b0382a4ccce9c6ee04b71a3
Shailav Malik <malikshailav@gmail.com>
2024-05-28 22:22:48 +0530

    Initial Commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,14 @@
+export const aboutMeData = [
+  {
+    id: 1,
+    bio: "Hello, My name is Shailav Malik. I am Full-Stack Developer & Design Enthusiast. I have worked on many projects in the last two years and learnt a lot.",
+  },
+  {
+  	id: 2,
+  	bio: 'My tech skills include HTML5, CSS3, JavaScript, TailwindCss, MaterialUI, ReactJs, NodeJS, MongoDB, MYSQL, ExpressJS, Laravel, Web Hosting,HTTP and REST, AI Coding.',
+  },
+  {
+  	id: 3,
+  	bio: 'I have worked with many clients in the last two years. I am known for my clean UI and scalable projects.',
+  },
+];
---
+++ src/data/clientsData.js
@@ -0,0 +1,20 @@
+// Import images
+
+import FilaImage from '../images/brands/fila_gray.png';
+import NBImage from '../images/brands/nb_gray.png';
+
+
+export const clientsHeading = 'Some of the brands that trust me';
+
+export const clientsData = [
+	{
+		id: 1,
+		title: 'FILA',
+		img: FilaImage,
+	},
+	{
+		id: 2,
+		title: 'NB',
+		img: NBImage,
+	},
+];
