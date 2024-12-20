79c393f6f09cd1439916d5872ce544eb848e9a82
Shailav Malik <malikshailav@gmail.com>
2024-12-20 13:37:24 +0530

    Backend added to handle form input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import { AnimatePresence } from 'framer-motion';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
-import AppFooter from './components/shared/AppFooter';
-import AppHeader from './components/shared/AppHeader';
+import ScrollToTop from './components/ScrollToTop.jsx';
+import AppFooter from './components/shared/AppFooter.jsx';
+import AppHeader from './components/shared/AppHeader.jsx';
 import './css/App.css';
-import UseScrollToTop from './hooks/useScrollToTop';
+import UseScrollToTop from './hooks/useScrollToTop.jsx';
 
-const About = lazy(() => import('./pages/AboutMe'));
+const About = lazy(() => import('./pages/AboutMe.jsx'));
 const Contact = lazy(() => import('./pages/Contact.jsx'));
-const Home = lazy(() => import('./pages/Home'));
-const Projects = lazy(() => import('./pages/Projects'));
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Projects = lazy(() => import('./pages/Projects.jsx'));
 const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
 
 
---
+++ frontend/src/hooks/useSendEmail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useSendEmail() {
+  
+    
+   
+}
+
+export default useSendEmail
